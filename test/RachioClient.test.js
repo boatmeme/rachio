a7af49b054e7b37449b3daed54b628bf3914929a
Jonathan Griggs <jonathan.griggs@gmail.com>
2017-07-12 14:11:51 -0600

    Implemented Unit Test Suite + nock-based fixtures

--- test/RachioClient.test.js
+++ test/RachioClient.test.js
@@ -1,20 +1,82 @@
 const should = require('should');
 const Rachio = require('../');
 const moment = require('moment');
+const _ = require('lodash');
+const { setupFixtures, teardownFixtures, Fixtures } = require('./fixtures');
+
+const Resource = require('../lib/resource/Resource');
+const Person = require('../lib/resource/Person');
+const CurrentConditions = require('../lib/resource/CurrentConditions');
+const Forecast = require('../lib/resource/Forecast');
+const Device = require('../lib/resource/Device');
+const Event = require('../lib/resource/Event');
+const Zone = require('../lib/resource/Zone');
+const CurrentSchedule = require('../lib/resource/CurrentSchedule');
 
 const apiToken = process.env.RACHIO_API_TOKEN;
+
+function validateZone(zone) {
+  zone.should.be.an.Object().instanceOf(Zone).and.instanceOf(Resource);
+  return zone;
+}
+
+function validateEvent(event) {
+  event.should.be.an.Object().instanceOf(Event).and.instanceOf(Resource);
+  return event;
+}
+
+function validateCurrentSchedule(currentSchedule) {
+  currentSchedule.should.be.an.Object().instanceOf(CurrentSchedule).and.instanceOf(Resource);
+  return currentSchedule;
+}
+
+function validateForecast(forecast) {
+  forecast.should.be.an.Object().instanceOf(Forecast).and.instanceOf(Resource);
+  return forecast;
+}
+
+function validateCurrentConditions(currentConditions) {
+  currentConditions.should.be.an.Object().instanceOf(CurrentConditions).and.instanceOf(Resource);
+  return currentConditions;
+}
+
+function validateDevice(device) {
+  device.should.be.an.Object().instanceOf(Device).and.instanceOf(Resource);
+  device.should.have.property('zones').is.an.Array();
+  device.zones.forEach(validateZone);
+  return device;
+}
+
+function validateError(error) {
+  error.should.be.an.Error();
+  return error;
+}
+
+function validateArray(validationFn, length) {
+  return arr => {
+    if (_.isNumber(length)) {
+      arr.should.be.an.Array().of.length(length);
+    } else {
+      arr.should.be.an.Array();
+    }
+    arr.forEach(validationFn);
+  };
+}
 
 describe('Rachio', () => {
   let client;
   let deviceId;
 
-  before(() => {
+  beforeEach(() => {
+    setupFixtures(Fixtures.Generic)();
     client = new Rachio(apiToken);
     return client.getDevices()
       .then(([device]) => {
         deviceId = device.id;
       });
   });
+
+  afterEach(teardownFixtures);
 
   describe('Person API', () => {
     describe('getPersonInfo', () => {
@@ -29,7 +91,7 @@ describe('Rachio', () => {
         client.getPersonInfo()
           .then(({ id }) => client.getPerson(id))
           .then(user => {
-            user.should.be.ok().and.an.Object();
+            user.should.be.ok().and.an.Object().instanceOf(Person);
           }));
     });
   });
@@ -37,61 +99,74 @@ describe('Rachio', () => {
     describe('getDevices', () => {
       it('should get the currently authenticated user\'s devices', () =>
         client.getDevices()
-          .then(devices => {
-            devices.should.be.an.Array();
-          }));
+          .then(validateArray(validateDevice)));
     });
+
     describe('getDeviceCurrentSchedule', () => {
       it('should get the current schedule for the specified device', () =>
         client.getDeviceCurrentSchedule(deviceId)
-          .then(schedule => {
-            schedule.should.be.an.Object();
-          }));
+          .then(validateCurrentSchedule));
     });
     describe('getDeviceEvents', () => {
-      it('should get the events between the start and end times for the specified device', () => {
-        const now = moment();
-        return client.getDeviceEvents(deviceId, now.subtract(7, 'days').valueOf(), now.add(7, 'days').valueOf())
-          .then(events => {
-            events.should.be.an.Array();
-          });
-      });
-      it('should get the events between the start and end times for the specified device using filters', () => {
-        const now = moment();
-        return client.getDeviceEvents(deviceId, now.subtract(7, 'days').valueOf(), now.add(7, 'days').valueOf(), { category: 'DEVICE' })
-          .then(events => {
-            events.should.be.an.Array();
-          });
-      });
+      const startTime = moment().subtract(7, 'days').valueOf();
+      const endTime = moment().subtract(3, 'days').valueOf();
+
+      it('should get the events between the start and end times for the specified device', () =>
+        client.getDeviceEvents(deviceId, startTime, endTime)
+          .then(validateArray(validateEvent, 77)));
+
+      it('should get the events between the start and end times for the specified device using filters', () =>
+        client.getDeviceEvents(deviceId, moment().subtract(1, 'days').valueOf(), Date.now(), { category: 'DEVICE' })
+          .then(validateArray(validateEvent, 10)));
+
+      it('should default to getting the last 28 days of events', () =>
+        client.getDeviceEvents(deviceId)
+          .then(validateArray(validateEvent, 193)));
+
+      it('should throw an error when trying to get events older than 28 days', () =>
+        client.getDeviceEvents(deviceId, moment().subtract(29, 'days').valueOf())
+          .catch(validateError));
     });
-    describe('getDeviceCurrentConditions', () => {
-      it('should get the current conditions for the specified device', () =>
-        client.getDeviceCurrentConditions(deviceId)
-          .then(conditions => {
-            conditions.should.be.an.Object();
-          }));
-    });
-    describe('getDeviceForecast', () => {
-      it('should get the forecast for the specified device', () =>
-        client.getDeviceForecast(deviceId)
-          .then(forecast => {
-            forecast.should.be.an.Array();
-          }));
-      it('should get today\'s forecast for the specified device on the given day', () =>
-        client.getDeviceForecastToday(deviceId)
-          .then(forecast => {
-            forecast.should.be.ok().and.an.Object();
-          }));
-      it('should get the forecast for the specified device on the given day', () =>
-        client.getDeviceForecast(deviceId, moment().add(1, 'day').valueOf())
-          .then(forecast => {
-            forecast.should.be.an.Array().of.length(1);
-          }));
-      it('should get the forecast for the specified device on the given days', () =>
-        client.getDeviceForecast(deviceId, moment().add(1, 'day').valueOf(), moment().add(4, 'day').valueOf())
-          .then(forecast => {
-            forecast.should.be.an.Array().of.length(3);
-          }));
+
+    describe('Forecasts and Conditions', () => {
+      before(setupFixtures(Fixtures.Forecast));
+      after(teardownFixtures);
+
+      describe('getDeviceCurrentConditions', () => {
+        describe('CurrentConditions.refresh', () => {
+          it('should refresh the current conditions', () =>
+            client.getDeviceCurrentConditions('RefreshCurrentConditions')
+              .then(validateCurrentConditions)
+              .then(currentConditions => currentConditions.refresh()
+                .then(validateCurrentConditions)
+                .then(conditionRefresh => {
+                  conditionRefresh.should.not.eql(currentConditions);
+                  conditionRefresh.localizedTimeStamp
+                    .should.be.greaterThan(currentConditions.localizedTimeStamp);
+                })));
+        });
+      });
+      describe('getDeviceForecast', () => {
+        it('should get the forecast for the specified device (14 days)', () =>
+          client.getDeviceForecast(deviceId)
+            .then(validateArray(validateForecast, 14)));
+
+        it('should get today\'s forecast for the specified device', () =>
+          client.getDeviceForecastToday(deviceId)
+            .then(validateForecast));
+
+        it('should get tomorrow\'s forecast for the specified device', () =>
+          client.getDeviceForecastTomorrow(deviceId)
+            .then(validateForecast));
+
+        it('should get the forecast for the specified device on the given day', () =>
+          client.getDeviceForecast(deviceId, moment().add(2, 'day').valueOf())
+            .then(validateArray(validateForecast, 1)));
+
+        it('should get the forecast for the specified device on the given days', () =>
+          client.getDeviceForecast(deviceId, moment().add(1, 'day').valueOf(), moment().add(4, 'day').valueOf())
+            .then(validateArray(validateForecast, 4)));
+      });
     });
   });
   describe('Zones API', () => {
@@ -103,7 +178,7 @@ describe('Rachio', () => {
           }));
     });
   });
-  describe('Webhooks API', () => {
+  describe.skip('Webhooks API', () => {
     describe('getWebhookTypes', () => {
       it('should get the available event types for webhooks', () =>
         client.getWebhookTypes()
---
+++ test/fixtures/index.js
@@ -0,0 +1,90 @@
+const nock = require('nock'); // eslint-disable-line
+const _ = require('lodash');
+
+const { StartOfDay } = require('../../lib/utils');
+const PersonInfo = require('./person_info.json');
+const Person = require('./person.json');
+const CurrentSchedule = require('./current_schedule.json');
+const Forecast = require('./forecast.json');
+const ForecastUpdate = require('./forecast_update.json');
+const Events = require('./events.json');
+const Device = require('./device.json');
+
+const forecastDateOffset = (StartOfDay(Date.now()) + 46800000) - 1499691600000;
+const eventDateOffset = Date.now() - 1499464193439;
+
+const OffsetForecast = data => {
+  Object.assign(data, {
+    current: Object.assign({},
+      data.current,
+      { localizedTimeStamp: data.current.localizedTimeStamp + forecastDateOffset }),
+    forecast: _.map(data.forecast,
+      f => Object.assign({},
+        f,
+        { localizedTimeStamp: f.localizedTimeStamp + forecastDateOffset })),
+  });
+};
+
+OffsetForecast(Forecast);
+OffsetForecast(ForecastUpdate);
+
+const getQuery = (uri = '') =>
+  _.reduce((_.last(uri.split('?')) || '').split('&'),
+    (acc, pair) => {
+      const [key, val] = pair.split('=');
+      return key && val ? Object.assign(acc, { [key]: val }) : acc;
+    },
+    {});
+
+const filterByBounds = (arr, prop, lower, upper) =>
+  _.filter(arr, o => o[prop] >= lower && o[prop] <= upper);
+
+const setupFixtures = fixture => () => {
+  nock.disableNetConnect();
+  fixture.call(null);
+};
+
+const teardownFixtures = () => () => {
+  nock.cleanAll();
+  nock.enableNetConnect();
+};
+
+const Fixtures = {};
+
+Fixtures.Generic = () => {
+  nock('https://api.rach.io')
+    .persist()
+    .get('/1/public/person/info')
+    .reply(200, PersonInfo)
+    .get('/1/public/device/2a5e7d3c-c140-4e2e-91a1-a212a518adc5')
+    .reply(200, Device)
+    .get('/1/public/person/c8d10892-fd69-48b3-8743-f111e4392d8a')
+    .reply(200, Person)
+    .get('/1/public/device/2a5e7d3c-c140-4e2e-91a1-a212a518adc5/current_schedule')
+    .reply(200, CurrentSchedule)
+    .get('/1/public/device/2a5e7d3c-c140-4e2e-91a1-a212a518adc5/event')
+    .query(true)
+    .reply(uri => {
+      const { startTime, endTime } = getQuery(uri);
+      return [200, filterByBounds(Events, 'eventDate', startTime - eventDateOffset, endTime - eventDateOffset)];
+    });
+};
+
+Fixtures.Forecast = () => {
+  nock('https://api.rach.io')
+    .get('/1/public/device/2a5e7d3c-c140-4e2e-91a1-a212a518adc5/forecast').times(5)
+    .query(true)
+    .reply(200, Forecast)
+    .get('/1/public/device/RefreshCurrentConditions/forecast')
+    .query(true)
+    .reply(200, Forecast)
+    .get('/1/public/device/RefreshCurrentConditions/forecast')
+    .query(true)
+    .reply(200, ForecastUpdate);
+};
+
+module.exports = {
+  setupFixtures,
+  teardownFixtures,
+  Fixtures,
+};
